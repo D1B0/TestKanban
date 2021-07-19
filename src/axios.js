8c115113ab57af3ba85625d158c0eaad9604b780
D1B0 <79042710+D1B0@users.noreply.github.com>
2021-07-19 18:48:46 +0300

    Delete src directory

--- src/axios.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseUrl= 'https://trello.backend.tests.nekidaem.ru/api/v1/cards/'
-axios.defaults.headers.common['Authorization']=`JWT ${localStorage.getItem('token')}`
